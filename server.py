a6a9666886a3c16833e2475b761550c821c1d0de
rikku-smile <nataliagg1212@gmail.com>
2025-01-13 15:32:46 -0500

    polish

--- server.py
+++ server.py
@@ -8,21 +8,19 @@ app = Flask("Emotion Detector")
 def emotion_detect():
     text_to_analyze = request.args.get('textToAnalyze')
     emotions = emotion_detector(text_to_analyze) 
-    anger = emotions["anger"]
-    disgust = emotions["disgust"]
-    fear = emotions["fear"]
-    joy = emotions["joy"]
-    sadness = emotions["sadness"]
     dom = emotions["dominant_emotion"]
-    common_msg = "For the given statement, the system response is "
-    msg = f"'anger': {anger}, 'disgust': {disgust}, 'fear': {fear}, 'joy': {joy}, 'sadness: {sadness}."
-    final_msg = f"The dominant emotion is {dom}"
+    msg = (
+        "For the given statement, the system response is "
+        f"'anger': {emotions['anger']}, 'disgust': {emotions['disgust']}, "
+        f"'fear': {emotions['fear']}, 'joy': {emotions['joy']}, "
+        f"'sadness': {emotions['sadness']}. The dominant emotion is {dom}."
+    )
 
-    return common_msg + msg + final_msg
+    return msg
 
 @app.route("/")
-def return_index_page():
-    render_template("index.html")
+def render_index_page():
+    return render_template("index.html")
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
